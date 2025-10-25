2cceb6869d74a0e97feee3b2a7298f692ebca635
Omar Hashad <omarhashad88@gmail.com>
2025-10-25 22:45:49 +0300

    Animation

--- My project/Assets/PlayerController.cs
+++ My project/Assets/PlayerController.cs
@@ -18,7 +18,14 @@ public class PlayerController : MonoBehaviour {
     public LayerMask whatIsGround; //this variable stores what is considered a ground to the character
     private bool grounded; //check if the character is standing on solid ground;
     // Use this for initialization
-	void Start () {
+
+    private Animator anim;
+
+
+	void Start ()
+     {
+        anim = GetComponent<Animator>();
+        
     }
 	
 	// Update is called once per frame
@@ -27,6 +34,7 @@ public class PlayerController : MonoBehaviour {
         if(Input.GetKeyDown(Spacebar) && grounded) //When user presses the space button ONCE
         {
             Jump(); //see function definition below   
+            
         }
        
         if (Input.GetKey(L)) //When user presses the left arrow button
@@ -42,15 +50,22 @@ public class PlayerController : MonoBehaviour {
       
         if (Input.GetKey(R)) //When user presses the left arrow button
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y); 
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
             //player character moves horizontally to the right along the x-axis without disrupting jump
 
             if(GetComponent<SpriteRenderer>()!=null)
             {
                 GetComponent<SpriteRenderer>().flipX = false;
-            }   
+            }
         }
+
+        anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+        anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
+        anim.SetBool("Grounded", grounded);
+
     }
+
+
 
     void FixedUpdate()
     {
@@ -60,7 +75,8 @@ public class PlayerController : MonoBehaviour {
 
     void Jump()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight); //player character jumps 
-        //vertically along the y-axis without disrupting horizontal walk      
+        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight); //player character jumps
+        //vertically along the y-axis without disrupting horizontal walk
     }
+
 }
